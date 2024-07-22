7d3f122b7e09a21659e9a93f7838b6f93d4f5954
Tosin Akinosho <takinosh@redhat.com>
2024-07-22 17:12:54 -0400

    chore: Update versioning script to fetch latest tag and increment by one

--- hack/version.sh
+++ hack/version.sh
@@ -3,13 +3,20 @@
 # Fetch the latest tag from git
 LATEST_TAG=$(git describe --tags "$(git rev-list --tags --max-count=1)")
 
-# Extract the numeric part of the tag (assuming tags are in the format v<NUMBER>)
-LATEST_VERSION=${LATEST_TAG//[!0-9]/}
+# Extract the numeric part of the tag (assuming tags are in the format v<MAJOR>.<MINOR>.<PATCH>)
+if [[ $LATEST_TAG =~ ^v([0-9]+)\.([0-9]+)\.([0-9]+)$ ]]; then
+  MAJOR=${BASH_REMATCH[1]}
+  MINOR=${BASH_REMATCH[2]}
+  PATCH=${BASH_REMATCH[3]}
 
-# Increment the latest version by one
-NEW_VERSION=$((LATEST_VERSION + 1))
+  # Increment the patch version by one
+  PATCH=$((PATCH + 1))
 
-# Format the new version as a tag (assuming the tag format is v<NUMBER>)
-NEW_TAG="v${NEW_VERSION}"
+  # Format the new version as a tag (assuming the tag format is v<MAJOR>.<MINOR>.<PATCH>)
+  NEW_TAG="v${MAJOR}.${MINOR}.${PATCH}"
+else
+  echo "Error: Latest tag does not match the expected format 'v<MAJOR>.<MINOR>.<PATCH>'"
+  exit 1
+fi
 
 echo $NEW_TAG
